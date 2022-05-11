1d7370df92916a0fd7a3018597034d4ea12d064e
Yukirin <skeith@users.noreply.github.com>
2022-05-11 22:50:43 +0800

    Kode untuk opsi 2

--- backupr.py
+++ backupr.py
@@ -9,16 +9,16 @@ log.set_verbosity(log.INFO)
 log.set_threshold(log.INFO)
 tgex = datetime.now()
 tgbak = tgex.strftime("%Y.%m.%d-%H.%M")
-# print statement debugging
-# print(tgex)
-# print(tgbak)
 
-def bakd(src, dest):
+def bakd():
     """Menerima dua parameter yaitu
     src: sumber folder yang ingin dibackup
     dest: lokasi tujuan backup
 
     Input harus berupa lokasi absolut"""
+    print("Gunakan lokasi absolut")
+    src = input("Lokasi folder sumber: ")
+    dest = input("Lokasi tujuan backup: ")
     try:
         if not src:
             print("Sumber folder yang ingin dibackup harus dimasukkan")
@@ -32,6 +32,39 @@ def bakd(src, dest):
         return
     except FileNotFoundError:
         print("Lokasi tidak ditemukan!\n Gunakan lokasi absolut")
+        return
+
+def bakf():
+    """Menerima empat parameter yaitu
+    src: sumber folder lokasi file
+    sercf: nama file yang ingin dibackup
+    dest: lokasi folder tujuan backup
+    destf: nama file hasil backup (opsional, apabila tidak diberikan maka menggunakan nama file sumber
+
+    Input folder harus berupa lokasi absolut
+    hasil backup otomatis ditambahkan timestamp"""
+    src = input("Lokasi folder sumber: ")
+    srcf = input("Nama file yang ingin dibackup (beserta extensi): ")
+    dest = input("Lokasi folder tujuan backup: ")
+    destf = input("Nama file tujuan backup (kosongkan jika ingin menggunakan nama yang sama): ")
+    pisah = "-"
+    try:
+        if not src and not srcf:
+            print("Sumber file yang ingin dibackup harus dimasukkan")
+            return
+        if not dest:
+            print("Lokasi tujuan backup harus dimasukkan")
+            return
+
+        # copy action
+        if not destf:
+            destf = srcf
+        fname, extname = os.path.splitext(destf)
+        shutil.copy2(os.path.join(src, srcf), os.path.join(dest,fname+pisah+tgbak+extname))
+        print("Backup sukses")
+        return
+    except FileNotFoundError:
+        print("Lokasi atau file tidak ditemukan!\n Gunakan lokasi absolut")
         return
 
 # menu
@@ -49,20 +82,11 @@ while True:
     show_menu()
     opt = int(input("Ketik opsi: "))
 
-# print("Pilih opsi backup\n")
-# print("1 : Backup folder")
-# print("2 : Backup file")
-# print("")
-
-# if else
     if opt == 1:
-        print("Gunakan lokasi absolut")
-        a = input("Lokasi folder sumber: ")
-        b = input("Lokasi tujuan backup: ")
-        bakd(a, b)
+        bakd()
     elif opt == 2:
-        print("Opsi belum siap")
+        bakf()
     elif opt == 3:
         exit()
     else:
-        print("Opsi tidak ada")
+        print("Opsi tersebut tidak ada")
